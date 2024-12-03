81307db2a770937d8da7636ec72a8545d43ebbbb
swyannnn <swyannnn@gmail.com>
2024-12-03 10:55:25 +0800

    move main to demo folder

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -1,4 +1,6 @@
-package com.example.demo.controller;
+package com.example.demo;
+
+import com.example.demo.controller.Controller;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
